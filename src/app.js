1838961ce166acc260e6d3726ef58d817fec913c
Bryan Navarro <diazbnavarro@gmail.com>
2025-09-23 17:52:46 +0000

    iterating throw objectkey array

--- src/app.js
+++ src/app.js
@@ -5,7 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+
+  console.log('Bryan');
+
+
+  let excuses = {
+    who: ['The dog', 'My grandma', 'The mailman', 'My bird'],
+    action: ['ate', 'peed', 'crushed', 'broke'],
+    what: ['my homework', 'my phone', 'the car'],
+    when: ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'],
+  }
+
+
+
+  function uno() {
+    console.log('por favor');
+
+  }
+
+  let ex_length = Object.keys(excuses);
+
+  for (const property in excuses) {
+    console.log('Hello');
+    console.log(excuses.property);
+    console.log(`${property}: ${excuses[property]}`);
+
+
+  }
+
+
+
+
+  window.uno = uno;
+
+
+
+
 };
